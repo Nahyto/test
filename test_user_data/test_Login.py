ff1b61bdd286742ebe20039d29ecbbd357232869
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 16:14:40 +0100

    Debut modif test_Login #2

--- test_user_data/test_Login.py
+++ test_user_data/test_Login.py
@@ -14,13 +14,15 @@ class testLogin(unittest.TestCase):
 
 	def test_Login(self):
 
-		fileAccessToken = open("fileAccessToken.txt","r")
-		print fileAccessToken.read()
+		fileData = open("fileAccessToken.txt","r")
+		catFileData = fileData.read()
+		access_token = catFileData.split("\n")[0]
+		user = catFileData.split("\n")[1]
 		params = (
  			('access_token', fileAccessToken.read()),	
 		)
 		addr = 'https://%s.citadel.team/_matrix/client/' %my_args[0]
-		dataLogin = '{"type":"m.login.password","user":"%s","password":"Devinemoi_01"}' % getUsername()
+		dataLogin = '{"type":"m.login.password","user":"%s","password":"Devinemoi_01"}' % user
 		requestLogin = requests.post('%sr0/login' % getAddr(), data=dataLogin, verify=False)
 		self.assertEquals(200,requestLogin.status_code)
 		print "\ntest_Login : \n\nVerify if the user can login in this homeserver"
