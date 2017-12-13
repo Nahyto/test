806a9976b8977d6e586ef2f4a0f8af626f8e8350
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 18:13:10 +0100

    Changement input #0

--- test_user_data/automate.py
+++ test_user_data/automate.py
@@ -6,7 +6,8 @@ def run_test():
 	verbose = 0
 	varAll = 0
 	test_file = []
-	infra = ""
+	email = ""
+	domain = ""
 
 	if len(sys.argv) == 1:
 		print "\nHello this is the automate-test 1.0 : If you want to execute some tests,\nYou can choose between this 2 options: \n    -v [Name of your test] : With that you can see the body response from the server\n    -a : With this you can run all tests\nIf you want to run one particular test, just write \"python automate.py --options [Name of your test]\".\nEnjoy ! \n"
@@ -16,28 +17,30 @@ def run_test():
 			if args == "-v":
 				verbose = 1
 			elif args == "-a":
-				varAll = 1
+				varAll = 2
 			elif args.endswith(".py"):
 				test_file.append(args)
 			else:
 				print "\nThere is a mistakes somewhere : check if you right correctly the options or the name of the test_file, and if the test_file is in the end of the arguments ! \n"
 
-		if varAll == 1:
+		if varAll == 2:
 			if len(test_file) != 0:
 				print "\nYou can't ask to execute one test if you select the options -a !\n"
 			else:
-				infra = raw_input("Write the name of your infra (Example: \"jla-test\") : ")
+				email = raw_input("Write the name of your email (Example: \"somebody@something.com\") : ")
+				domain = subprocess.check_output("./get_domain.sh %s" %email,shell=True)
 				for test in os.listdir("."):
 					if test != "automate.py":
-						subprocess.call("python %s" %(test),shell=True)
+						subprocess.call("python %s %s %s" %(test,email,domain),shell=True)
 
 		else:
 			if len(test_file) == 0:
 				print "\nIf you didn't ask for running all tests, you have to write a test_file\n"
 			else:
-				infra = raw_input("Write the name of your infra (Example: \"jla-test\") : ")
+				email = raw_input("Write the name of your email (Example: \"somebody@something.com\") : ")
+				domain = subprocess.check_output("./get_domain.sh %s" %email,shell=True)
 				for test in test_file:
-					subprocess.call("python %s %s %s" %(test,infra,verbose),shell=True)
+					subprocess.call("python %s %s %s %s" %(test,email,verbose,domain),shell=True)
 
 
 if __name__ == '__main__':
--- test_user_data/test_Register.py
+++ test_user_data/test_Register.py
@@ -5,7 +5,6 @@ import urllib3
 import unittest
 import sys
 import subprocess
-from attribute import getUsername
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 my_args = sys.argv[1:]
@@ -15,42 +14,58 @@ class testRegister(unittest.TestCase):
 
 	def test_Register(self):
 
+		verbose = ""
+		email = ""
+		domain = ""
+		infra = ""
+		if len(my_args) == 3:
+			verbose = my_args[1]
+			email = my_args[0]
+			domain = my_args[2]
+
+		elif len(my_args) == 2:
+			email = my_args[0]
+			domain = my_args[1]
+
+		infra = domain.split(".")[0]
+		username = email.split("@")[0]
+
 		headers = {
  	   		'Content-Type': 'application/json',
     		'Accept': 'application/json',
 		}
 
-		addr = 'https://%s.citadel.team/_matrix/client/' %my_args[0]
+		addr = 'https://%s/_matrix/client/' %domain
 
-		dataRegisterEmail = '{"client_secret":"abcd","id_server":"%s.citadel.team","send_attempt":"1","email":"%s@outlook.fr","next_link":""}' %(my_args[0],getUsername())
+		dataRegisterEmail = '{"client_secret":"abcd","id_server":"%s","send_attempt":"1","email":"%s","next_link":""}' %(domain,email)
 		requestRegisterEmail = requests.post('%sr0/register/email/requestToken' %addr, headers=headers, data=dataRegisterEmail)
 		self.assertEquals(200,requestRegisterEmail.status_code)
 		bodyEmail = (requestRegisterEmail.text).split('"')
 		sid = bodyEmail[5]
 		
-		domain = (my_args[0]).split("-")
-		registerToken = subprocess.check_output("ssh -i ~/team-playbook/ssh/id_rsa ansible@back-%s.tcs-citadeldev.cloud-omc.org \"docker exec sydent-container sqlite3 /opt/sydent/database/sydent.db 'select * from threepid_token_auths where validationSession=%s'\" | cut -d'|' -f3" %(domain[0],sid),shell=True).strip()
+		domain = (test).split("-")
+		registerToken = subprocess.check_output("ssh -i ~/team-playbook/ssh/id_rsa ansible@back-%s.tcs-citadeldev.cloud-omc.org \"docker exec sydent-container sqlite3 /opt/sydent/database/sydent.db 'select * from threepid_token_auths where validationSession=%s'\" | cut -d'|' -f3" %(infra,sid),shell=True).strip()
 		params = (
     		('token', '%s' %registerToken),
     		('client_secret', 'abcd'),
    	 		('sid', '%s' %sid),
 		)
-		requestValidate = requests.post('https://%s.citadel.team/_matrix/identity/api/v1/validate/email/submitToken' %my_args[0], params=params)
+		requestValidate = requests.post('https://%s/_matrix/identity/api/v1/validate/email/submitToken' %domain, params=params)
 		self.assertIn("true",requestValidate.text)
 
-		dataRegisterUser = '{"auth": {"type": "m.login.email.identity","threepid_creds":{"id_server": "%s.citadel.team","sid": "%s","client_secret": "abcd"}},"bind_email": true,"password": "Devinemoi_01","username": "%s"}' %(my_args[0],sid,getUsername())
-		requestRegisterUser = requests.post('https://%s.citadel.team/_matrix/client/r0/register' %my_args[0], headers=headers, data=dataRegisterUser, verify=True)
+		dataRegisterUser = '{"auth": {"type": "m.login.email.identity","threepid_creds":{"id_server": "%s","sid": "%s","client_secret": "abcd"}},"bind_email": true,"password": "Devinemoi_01","username": "%s"}' %(domain,sid,username)
+		requestRegisterUser = requests.post('https://%s.citadel.team/_matrix/client/r0/register' %test, headers=headers, data=dataRegisterUser, verify=True)
 		self.assertEquals(200,requestRegisterUser.status_code)
 		bodyUser = (requestRegisterUser.text).split("\"")
 		access_token = bodyUser[3]
 
 		fileAccessToken = open("fileAccessToken.txt","w")
 		fileAccessToken.write(access_token)
-		fileAccessToken.write("\n%s" %getUsername())
+		fileAccessToken.write("\n%s" %username)
 		fileAccessToken.close()
 
 		print "\ntest_Register : \n\nRegister for an account on this homeserver.\nThere are two kinds of user account:\n    -user accounts. These accounts may use the full API described in this specification. \n    -guest accounts. These accounts may have limited permissions and may not be supported by all servers."
-		if my_args[1] == '1':
+		if verbose == '1':
 			print "\nResponse server :\n%s\n" %requestRegisterUser.text
 
 
