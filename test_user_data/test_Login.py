19737bd9f2b202dcd40eb3f138a2182daba99799
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 16:06:53 +0100

    Debut modif test_Login

--- test_user_data/test_Login.py
+++ test_user_data/test_Login.py
@@ -4,17 +4,23 @@ import requests
 import urllib3
 import unittest
 import sys
+from attribute import getUsername
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+my_args = sys.argv[1:]
+del sys.argv[1:]
 
 class testLogin(unittest.TestCase):
 
 	def test_Login(self):
 
-		urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+		fileAccessToken = open("fileAccessToken.txt","r")
+		print fileAccessToken.read()
 		params = (
- 			('access_token', getAccesToken()),	
+ 			('access_token', fileAccessToken.read()),	
 		)
-		dataLogin = '{"type":"m.login.password","user":"%s","password":"devine","session":"bidule"}' % getUsername()
+		addr = 'https://%s.citadel.team/_matrix/client/' %my_args[0]
+		dataLogin = '{"type":"m.login.password","user":"%s","password":"Devinemoi_01"}' % getUsername()
 		requestLogin = requests.post('%sr0/login' % getAddr(), data=dataLogin, verify=False)
 		self.assertEquals(200,requestLogin.status_code)
 		print "\ntest_Login : \n\nVerify if the user can login in this homeserver"
