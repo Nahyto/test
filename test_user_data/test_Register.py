359deb378593560624f99aa2dfa4a89614a0c41d
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 12:46:14 +0100

    Fin register

--- test_user_data/test_Register.py
+++ test_user_data/test_Register.py
@@ -23,22 +23,25 @@ class testRegister(unittest.TestCase):
 
 		addr = 'https://%s.citadel.team/_matrix/client/' %my_args[0]
 
-		dataRegister = '{"client_secret":"abcd","id_server":"%s.citadel.team","send_attempt":"1","email":"%s@outlook.fr","next_link":""}' %(my_args[0],getUsername())
-		requestRegister = requests.post('%sr0/register/email/requestToken' %addr, headers=headers, data=dataRegister)
-		self.assertEquals(200,requestRegister.status_code)
-		body = (requestRegister.text).split('"')
+		dataRegisterEmail = '{"client_secret":"abcd","id_server":"%s.citadel.team","send_attempt":"1","email":"%s@outlook.fr","next_link":""}' %(my_args[0],getUsername())
+		requestRegisterEmail = requests.post('%sr0/register/email/requestToken' %addr, headers=headers, data=dataRegisterEmail)
+		self.assertEquals(200,requestRegisterEmail.status_code)
+		body = (requestRegisterEmail.text).split('"')
 		sid = body[5]
 		
 		domain = (my_args[0]).split("-")
 		registerToken = subprocess.check_output("ssh -i ~/team-playbook/ssh/id_rsa ansible@back-%s.tcs-citadeldev.cloud-omc.org \"docker exec sydent-container sqlite3 /opt/sydent/database/sydent.db 'select * from threepid_token_auths where validationSession=%s'\" | cut -d'|' -f3" %(domain[0],sid),shell=True).strip()
 		params = (
-    			('token', '%s' %registerToken),
-    			('client_secret', 'abcd'),
+    		('token', '%s' %registerToken),
+    		('client_secret', 'abcd'),
    	 		('sid', '%s' %sid)
 		)
-		print registerToken
 		requestValidate = requests.post('https://%s.citadel.team/_matrix/identity/api/v1/validate/email/submitToken' %my_args[0], params=params)
 		self.assertIn("true",requestValidate.text)
+
+		dataRegisterUser = '{"auth": {"type": "m.login.email.identity","threepid_creds":{"id_server": "%s.citadel.team","sid": "%s","client_secret": "abcd"}},"bind_email": true,"password": "Devinemoi_01","username": "%s"}' %(my_args[0],sid,getUsername())
+		requestRegisterUser = requests.post('https://%s.citadel.team/_matrix/client/r0/register' %my_args[0], headers=headers, data=dataRegisterUser, verify=True)
+		self.assertEquals(200,requestRegisterEmail.status_code)
 
 		print "\ntest_Register : \n\nRegister for an account on this homeserver.\nThere are two kinds of user account:\n    -user accounts. These accounts may use the full API described in this specification. \n    -guest accounts. These accounts may have limited permissions and may not be supported by all servers."
 		if my_args[1] == '1':
