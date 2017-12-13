d745813954f51c4727eeb6548b486f3b3dbe51f5
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 16:31:36 +0100

    Modif test_Logout #0

--- test_user_data/test_Logout.py
+++ test_user_data/test_Logout.py
@@ -10,14 +10,16 @@ class testLogout(unittest.TestCase):
 
 	def test_Logout(self):
 
-		urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
+		fileData = open("fileAccessToken.txt","r")
+		catFileData = fileData.read()
+		access_token = catFileData.split("\n")[0]
 
 		params = (
-    		('access_token', getAccesToken()),
+    		('access_token', access_token)
 		)
 
-		requestLogout = requests.post('%sr0/logout' % getAddr(), params=params, verify=False)
+		addr = 'https://%s.citadel.team/_matrix/client/' %my_args[0]
+		requestLogout = requests.post('%sr0/logout' %addr, params=params, verify=True)
 		self.assertEquals(200,requestLogout.status_code)
 		print "\ntest_Logout: \n\nVerify if the user can Logout from this homeserver."
 
