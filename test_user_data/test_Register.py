60ee71ef6f9aeaaeefcbf54a2f71e9394e1b304c
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 16:10:40 +0100

    Debut modif test_Login #1

--- test_user_data/test_Register.py
+++ test_user_data/test_Register.py
@@ -10,7 +10,7 @@ from attribute import getUsername
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 my_args = sys.argv[1:]
 del sys.argv[1:]
-access_token = "lol"
+
 class testRegister(unittest.TestCase):
 
 	def test_Register(self):
@@ -43,9 +43,12 @@ class testRegister(unittest.TestCase):
 		self.assertEquals(200,requestRegisterUser.status_code)
 		bodyUser = (requestRegisterUser.text).split("\"")
 		access_token = bodyUser[3]
+
 		fileAccessToken = open("fileAccessToken.txt","w")
 		fileAccessToken.write(access_token)
+		fileAccessToken.write("\n%s" %getUsername())
 		fileAccessToken.close()
+
 		print "\ntest_Register : \n\nRegister for an account on this homeserver.\nThere are two kinds of user account:\n    -user accounts. These accounts may use the full API described in this specification. \n    -guest accounts. These accounts may have limited permissions and may not be supported by all servers."
 		if my_args[1] == '1':
 			print "\nResponse server :\n%s\n" %requestRegisterUser.text
