11472ff1da727348c5df7ef70fb4f358820bb890
Nelson Francois <nelsonf@citadel-cc-dev.localdomain>
2017-12-13 12:33:20 +0100

    Fin validate

--- test_user_data/test_Register.py
+++ test_user_data/test_Register.py
@@ -26,20 +26,19 @@ class testRegister(unittest.TestCase):
 		dataRegister = '{"client_secret":"abcd","id_server":"%s.citadel.team","send_attempt":"1","email":"%s@outlook.fr","next_link":""}' %(my_args[0],getUsername())
 		requestRegister = requests.post('%sr0/register/email/requestToken' %addr, headers=headers, data=dataRegister)
 		self.assertEquals(200,requestRegister.status_code)
-		
 		body = (requestRegister.text).split('"')
 		sid = body[5]
 		
 		domain = (my_args[0]).split("-")
-		registerToken = subprocess.check_output("ssh -i ~/team-playbook/ssh/id_rsa ansible@back-%s.tcs-citadeldev.cloud-omc.org \"docker exec sydent-container sqlite3 /opt/sydent/database/sydent.db 'select * from threepid_token_auths where validationSession=%s'\" | cut -d'|' -f3" %(domain[0],sid),shell=True)
+		registerToken = subprocess.check_output("ssh -i ~/team-playbook/ssh/id_rsa ansible@back-%s.tcs-citadeldev.cloud-omc.org \"docker exec sydent-container sqlite3 /opt/sydent/database/sydent.db 'select * from threepid_token_auths where validationSession=%s'\" | cut -d'|' -f3" %(domain[0],sid),shell=True).strip()
 		params = (
-    		('token', registerToken),
-    		('client_secret', 'abcd'),
-    		('sid', sid),
+    			('token', '%s' %registerToken),
+    			('client_secret', 'abcd'),
+   	 		('sid', '%s' %sid)
 		)
-
+		print registerToken
 		requestValidate = requests.post('https://%s.citadel.team/_matrix/identity/api/v1/validate/email/submitToken' %my_args[0], params=params)
-		self.assertIn("True",requestValidate.text)
+		self.assertIn("true",requestValidate.text)
 
 		print "\ntest_Register : \n\nRegister for an account on this homeserver.\nThere are two kinds of user account:\n    -user accounts. These accounts may use the full API described in this specification. \n    -guest accounts. These accounts may have limited permissions and may not be supported by all servers."
 		if my_args[1] == '1':
