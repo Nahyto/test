ef74a83cf6c9bb746b668643b2ddd98f38422c23
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-13 18:15:25 +0100

    Oublié le get_domain

---
+++ test_user_data/get_domain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+date=$(date +%Y%m%d)
+
+
+data=$(curl 'https://ptl-test.citadel.team/api/get_domain.php?email='$1)
+if [ $data = null ]; then
+	echo "NOK"
+>> /dev/null
+else
+	domain=$(echo $data | cut -d'"' -f4)
+	echo $domain
+fi
