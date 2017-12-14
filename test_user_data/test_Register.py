2e864832ae1c7f5d049c802a03ff8e1dbeda8aa9
Nelson Francois <nelsonf@citadel-cc-dev.localdomain>
2017-12-14 15:03:04 +0100

    La beauté n'est qu'un défaut que la société veut valoriser #6

--- test_user_data/test_Register.py
+++ test_user_data/test_Register.py
@@ -12,16 +12,15 @@ class testRegister(unittest.TestCase):
 
 	def test_Register_Email(self):
 		global requestRegisterEmail
-
 		dataRegisterEmail = '{"client_secret":"abcd","id_server":"%s","send_attempt":"1","email":"%s","next_link":""}' %(getDomain(),getEmail())
 
 		requestRegisterEmail = requests.post('%sclient/r0/register/email/requestToken' %getAddr(), headers=getHeader(), data=dataRegisterEmail)
 
 		self.assertEquals(200,requestRegisterEmail.status_code)
 
-	def test_Register_User(self):
+	def test_Wegister_User(self):
 		dataRegisterUser = '{"auth": {"type": "m.login.email.identity","threepid_creds":{"id_server": "%s","sid": "%s","client_secret": "abcd"}},"bind_email": true,"password": "Devinemoi_01","username": "%s"}' %(getDomain(),sid,getUsername())
-		requestRegisterUser = requests.post('%s/_matrix/client/r0/register' %getAddr(), headers=headers, data=dataRegisterUser, verify=True)
+		requestRegisterUser = requests.post('%sclient/r0/register' %getAddr(), headers=getHeader(), data=dataRegisterUser, verify=True)
 		self.assertEquals(200,requestRegisterUser.status_code)
 		bodyUser = (requestRegisterUser.text).split("\"")
 		access_token = bodyUser[3]
@@ -33,7 +32,7 @@ class testRegister(unittest.TestCase):
 		print "\ntest_Register : \n\nRegister for an account on this homeserver.\nThere are two kinds of user account:\n    -user accounts. These accounts may use the full API described in this specification. \n    -guest accounts. These accounts may have limited permissions and may not be supported by all servers."
 		if getVerbose() == '1':
 			print "\nResponse server :\n%s\n" %requestRegisterUser.text
-
+		
 	def test_Validate_Email(self):
 		global sid
 
