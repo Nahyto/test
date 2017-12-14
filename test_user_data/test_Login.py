213c944b4bda4605bd4250f3fb42b11bef7575a4
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-14 15:40:06 +0100

    La beauté n'est qu'un défaut que la société veut valoriser #9

--- test_user_data/test_Login.py
+++ test_user_data/test_Login.py
@@ -4,43 +4,32 @@ import requests
 import urllib3
 import unittest
 import sys
-
-my_args = sys.argv[1:]
-del sys.argv[1:]
+from attribute import *
 
 class testLogin(unittest.TestCase):
 
 	def test_Login(self):
-		verbose = ""
-		email = ""
-		domain = ""
-		infra = ""
-		if len(my_args) == 3:
-			verbose = my_args[1]
-			email = my_args[0]
-			domain = my_args[2]
 
-		elif len(my_args) == 2:
-			email = my_args[0]
-			domain = my_args[1]
-		username = email.split("@")[0]
-
-		fileDataRead = open("fileAccessToken.txt","r")
-		catFileData = fileDataRead.read()
-		access_token = catFileData.split("\n")[0]
 		params = (
- 			('access_token', access_token),	
+
+ 			('access_token', getAccessToken()),	
+		
 		)
-		addr = 'https://%s/_matrix/client/' %domain
-		dataLogin = '{"type":"m.login.password","user":"%s","password":"Devinemoi_01"}' % username
-		requestLogin = requests.post('%sr0/login' %addr, data=dataLogin, verify=True)
+
+		dataLogin = '{"type":"m.login.password","user":"%s","password":"Devinemoi_01"}' % getUsername()
+		requestLogin = requests.post('%sr0/login' %getAddr(), data=dataLogin, verify=True)
+
+
 		self.assertEquals(200,requestLogin.status_code)
 
-		fileDataWrite = open("fileLoginToken.txt","w")
 		body = (requestLogin.text).split("\"")
-		fileDataWrite.write(body[3])
-		fileDataWrite.write("\n")
-		print "\ntest_Login : \n\nVerify if the user can login in this homeserver"
+		login_token = body[3]
+
+		setLoginToken(login_token)
+
+		
 
 if __name__ == '__main__':
+	print "\n\n\033[1;32;40mtest_Login : \n\n\033[1;32;m"
+	print "Verify if the user can login in this homeserver."
 	unittest.main()
