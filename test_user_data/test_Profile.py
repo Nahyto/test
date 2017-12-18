4f09970a08901a66bdd9f8e0566b8aa9e005064e
Nelson Francois <nelsonf@citadel-cc-dev.localdomain>
2017-12-18 11:01:44 +0100

    Test admin et client/ Displayname & Profile #1

--- test_user_data/test_Profile.py
+++ test_user_data/test_Profile.py
@@ -1,13 +1,21 @@
 import requests
 import urllib3
 import unittest
+import urllib
 from attribute import *
 
 class testProfile(unittest.TestCase):
 
 	def test_Profile(self):
+		
+		user_id = "@" + getUsername() + ":" + getDomain()
+		user_id = urllib.quote("%s" % (user_id))
+		print user_id
+		requestProfile = requests.get('%sclient/r0/profile/%s' %(getAddr(),user_id), verify=True)
+		
+		if getVerbose() == '1':
+			print "\n\033[1;32;40mResponse server :\033[1;32;36m\n%s\n\n\033[1;32;m" %requestProfile.text
 
-		requestProfile = requests.get('%sclient/r0/profile/%s' %(getAddr(),getUserID()), verify=False)
 		self.assertEquals(200,requestProfile.status_code)
 		print "\ntest_Profile : \n Get the combined profile information for this user."
 
