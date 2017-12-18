1f24afbfc6c3fcb302090919e3b1b490d601b15b
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-18 11:33:54 +0100

    La beauté n'est qu'un défaut que la société veut valoriser #34

--- test_user_data/test_Logout.py
+++ test_user_data/test_Logout.py
@@ -6,14 +6,14 @@ import urllib
 import sys
 import unittest
 import os
+import paramiko
 import subprocess
 from attribute import *
 
 class testLogout(unittest.TestCase):
 
-	def test_Logout(self):
-
-		"""params = (
+	"""def test_Logout(self):
+			params = (
     			('access_token', getAccessToken()),
 		)
 		user_id = "@" + getUsername() + ":" + getDomain()
@@ -26,10 +26,31 @@ class testLogout(unittest.TestCase):
 			print "\n\033[1;32;40mResponse server :\033[1;32;36m\n%s\n\n\033[1;32;m" %requestLogout.text
 
 		self.assertEquals(200,requestLogout.status_code)"""
-		user_id = "@" + getUsername() + ":" + getDomain()
-		user_id = urllib.quote("'%s'" % (user_id))
-		test = subprocess.check_output(["ssh","-i","~/team-playbook/ssh/id_rsa","ansible@back-jla.tcs-citadeldev.cloud-omc.org","\"docker exec -t bdd-container psql synapse --command 'SELECT token FROM access_tokens WHERE user_id = %s;'\""])
-		print test
+
+	client = None
+ 
+    def __init__(self):
+    	k = paramiko.RSAKey.from_private_key_file("~/team-playbook/ssh/id_rsa")
+        print("Connecting to server.")
+        self.client = client.SSHClient()
+        self.client.set_missing_host_key_policy(client.AutoAddPolicy())
+        self.client.connect("back-jla.tcs-citadeldev.cloud-omc.org", username='ansible', pkey = k)
+ 
+    def sendCommand(self):
+        if(self.client):
+            stdin, stdout, stderr = self.client.exec_command("docker exec -t bdd-container psql synapse --command \"SELECT token FROM access_tokens WHERE user_id = '@nelsonthales59147:jla-test.citadel.team';\"")
+            while not stdout.channel.exit_status_ready():
+                # Print data when available
+                if stdout.channel.recv_ready():
+                    alldata = stdout.channel.recv(1024)
+                    prevdata = b"1"
+                    while prevdata:
+                        prevdata = stdout.channel.recv(1024)
+                        alldata += prevdata
+ 
+                    print(str(alldata, "utf8"))
+        else:
+            print("Connection not opened.")
 		
 
 if __name__ == '__main__':
