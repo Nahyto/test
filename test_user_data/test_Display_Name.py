2c8ac8a5cd0dcfa09fad28919f461d1aac422f5e
Nelson_F <nelson.francois@thalesgroup.com>
2017-12-18 15:35:10 +0100

    Test admin et client/ Displayname & Profile #5

--- test_user_data/test_Display_Name.py
+++ test_user_data/test_Display_Name.py
@@ -1,30 +1,34 @@
 import requests
 import urllib3
 import unittest
-from test_Register import getAccesToken,getUserID
-from attribute import getUsername,getAddr
+from attribute import *
 
 class testDisplayName(unittest.TestCase):
 
 	def test_Display_Name(self):
 
-		headers = {
-   			'Content-Type': 'application/json',
-    		'Accept': 'application/json',
-		}
+		user_id = "@" + getUsername() + ":" + getDomain()
+		user_id = urllib.quote("%s" % (user_id))
 
 		params = (
-    		('access_token', getAccesToken()),
+    		('access_token', getAccessToken()),
 		)
 
 		dataDisplayName = '{"displayname":"Jean richard"}'
-		requestPutDisplayName = requests.put('%sr0/profile/%s/displayname' %(getAddr(),getUserID()), headers=headers, params=params,data=dataDisplayName, verify=False)
+		requestPutDisplayName = requests.put('%sclient/r0/profile/%s/displayname' %(getAddr(),user_id), headers=getHeader(), params=params,data=dataDisplayName, verify=True)
 		self.assertEquals(200,requestPutDisplayName.status_code)
 
-		requestGetDisplayName = requests.get('%sr0/profile/%s/displayname' %(getAddr(),getUserID()), verify=False)
+		requestGetDisplayName = requests.get('%sclient/r0/profile/%s/displayname' %(getAddr(),user_id), verify=True)
 		self.assertEquals(200,requestGetDisplayName.status_code)
-		print "\ntest_Display_Name : \n\nPut and Get the user's display name."
+
+		dataDisplayName = '{"displayname":"Jean Armand"}'
+		requestPutDisplayName = requests.put('%sclient/r0/profile/%s/displayname' %(getAddr(),user_id), headers=getHeader(), params=params,data=dataDisplayName, verify=True)
+		self.assertEquals(200,requestPutDisplayName.status_code)
+
+
 
 
 if __name__ == '__main__':
+	print "\ntest_Display_Name : \n"
+	print "Put and Get the user's display name."
 	unittest.main()
